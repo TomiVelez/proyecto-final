c78c35e305c11b48352e10e94fee0cb7d37dad47
TomiVelez <tgroesman@gmail.com>
2025-11-05 20:11:55 -0300

    Update michat.py

--- michat.py
+++ michat.py
@@ -7,7 +7,7 @@ nombre = st.text_input("Cual es tu nombre?")
 if st.button("Saludar!"):
     st.write(f"Hola {nombre}! Bienvenido a talento tech")
 
-MODELOS = ['llama3-8b-8192', 'llama3-70b-8192', 'mixtral-8x7b-32768']
+MODELOS = ['llama-3.1-8b-instant', 'llama-3.3-70b-versatile']
 
 def configurar_pagina():
     st.title("Mi Chat de IA")
@@ -28,7 +28,7 @@ def configurar_modelo(cliente, modelo, mensajeDeEntrada):
     return cliente.chat.completions.create(
         model = modelo,
         messages = [{"role":"user", "content": mensajeDeEntrada}],
-        stream = False
+        stream = True
     )
 
 def inicializar_estado():
@@ -44,28 +44,33 @@ def mostrar_historial():
 def area_chat():
     contenedorDelChat = st.container(height=400, border= True)
     with contenedorDelChat: mostrar_historial()
+def generar_respuestas(chat_completo):
+    respuesta_completa = ""
+    for frase in chat_completo:
+        print(frase.choices[0].delta.content)
+        if frase.choices[0].delta.content:
+            respuesta_completa += frase.choices[0].delta.content
+            yield frase.choices[0].delta.content
+    return respuesta_completa
+
+# Main - > Todas las funciones para correr el Chatbot
+def main ():
+    clienteUsuario = crear_usuario_groq()
+    inicializar_estado()
+    modelo = configurar_pagina()
+    area_chat() #Nuevo 
+    mensaje = st.chat_input("Escribi tu mensaje:")
+
+    if mensaje:
+        actualizar_historial("user", mensaje, "😁")
+        chat_completo = configurar_modelo(clienteUsuario, modelo, mensaje)
+        if chat_completo:
+                with st.chat_message("assistant"):
+                    respuesta_completa = st.write_stream(generar_respuestas(chat_completo))
+                    actualizar_historial("assistant", respuesta_completa, "🤖")
+                    st.rerun()
 
 
-clienteUsuario = crear_usuario_groq()
-inicializar_estado()
-area_chat()
-modelo = configurar_pagina()
-mensaje = st.chat_input("Escribi tu mensaje:")
-if mensaje:
-    actualizar_historial("user", mensaje, "😁")
-    chat_completo = configurar_modelo(clienteUsuario, modelo, mensaje)
-    actualizar_historial("assistant", chat_completo, "🤖")
-    st.rerun()
 
-
-if mensaje:
-    configurar_modelo(clienteUsuario, modelo, mensaje)
-    print(mensaje)
-
-
-
-modelo = configurar_pagina()
-mensaje = st.chat_input("Escribi tu mensaje")
-st.set_page_config("Tu propio chatbot")
-st.title("Tu primer chat de Streamlit")
-st.sidebar.title("Mi chat del cole")
+if __name__ == "__main__":
+    main()
